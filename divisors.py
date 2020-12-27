3e68502064ae7a5405598af93b2cfc2e813c3f67
UselessToaster <75389500+UselessToaster@users.noreply.github.com>
2020-12-26 21:49:21 -0500

    Update divisors.py

--- divisors.py
+++ divisors.py
@@ -1,6 +1,8 @@
-print("pick a number, any number :)")
+#gives the factors of a chosen number from 1-100 
+
+print("Pick a number, any number! :)")
+
 x = int(input())
 num_list = range(1, 101)
-print("these are the divisors of {}: ".format(x), [num for num in num_list if x % num == 0])
-  
-  
+ 
+print("These are the divisors of {}: ".format(x), [num for num in num_list if x % num == 0])
