09498151727b29c10ebfa497fe438dbafe8085c4
Tianna <75389500+UselessToaster@users.noreply.github.com>
2021-01-03 10:43:30 -0500

    Add files via upload

---
+++ Check Primality functions.py
@@ -0,0 +1,29 @@
+### determines prime numbers ###
+
+# gets user input
+def user_input():
+    return int(input("Give me a number and I will determine its primality: "))
+user = user_input()
+
+
+def total_factors(x):
+    # gets factors of user input
+    num_list = range(1, user+1)
+    factors = [num for num in num_list if user % num == 0]
+    print("These are the factors of {}:".format(user), factors)
+
+    # counts number of factors
+    for elem in factors:
+        x += 1
+    return x
+total = total_factors(0)
+
+
+# determines primality
+def primality():
+    if total == 2:
+        print("{} is only divisible by 1 and itself so it is a prime number.".format(user))
+
+    else:
+        print("{} is divisible by 1, itself, and {} other numbers, so it is not prime.".format(user, total-2))
+primality()
